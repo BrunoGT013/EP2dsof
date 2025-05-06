be2ee66e0b7cf4fb49d3d4714fe8aa3b3bbc1b57
BrunoGT013 <brunogt1@al.insper.edu.br>
2025-05-06 08:21:37 -0300

    Guardar dados.py

--- EP2.py
+++ EP2.py
@@ -5,3 +5,9 @@ def rolar_dados(quantidade):
     for i in range(quantidade):
         resultado.append(random.randint(1, 6))
     return resultado
+
+
+def guardar_dado(lista_rolados, lista_estoque, indice_dado):
+    dado_escolhido = lista_rolados[indice_dado]
+    lista_estoque.append(dado_escolhido)
+    return [lista_rolados, lista_estoque]
